965375d8a11110c8fae422df30d50000c331ad1f
Cedric David <cedric.david@btopenworld.com>
2018-08-28 13:08:59 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -15,8 +15,9 @@ def valid_move?(board, index)
   else
     if index < 1 || index > 9
       false
-    else true
+    else 
+      true
+    end
   end
 end
-
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
